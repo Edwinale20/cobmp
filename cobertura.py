2a8ec759c1c4bbaf035f841ab58759e034d4016c
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-11-24 01:15:34 -0600

    Update cobertura.py

--- cobertura.py
+++ cobertura.py
@@ -97,7 +97,7 @@ def venta(venta_semanal):
 
     columnas_a_eliminar = [col for col in concat_venta.columns if 'Unnamed' in col] + ['Metrics']
     concat_venta = concat_venta.drop(columns=columnas_a_eliminar, errors='ignore')
-    concat_venta['Semana Contable'] = concat_venta['Semana Contable'].astype(str)
+    #concat_venta['Semana Contable'] = concat_venta['Semana Contable'].astype(str)
     concat_venta['Unidades inventario'] = concat_venta['Unidades inventario'].astype('int64')
 
     concat_venta = concat_venta.rename(columns={
